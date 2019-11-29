ee9090be82b11bfe45b5402aff3e9f8b30f02469
rcastro33 <rcastro@programmer.net>
2019-11-28 21:32:18 -0300

    Deleted main, there is not main in Ruby. Added an other job_class (Job_freak_print)  thats inherit  the main Job_print class

--- job_classes.rb
+++ job_classes.rb
@@ -1,6 +1,6 @@
 #require ...
 
-class Job_one
+class Job_print
 
     # Class variables
 
@@ -16,31 +16,63 @@ class Job_one
     def exec_now
         @job_id
         @sync=true
-        puts "Exec_now llamado con éxito.\nSync vale: #@sync "
+        # puts "Exec_now llamado con éxito.\nSync vale: #@sync "
         @job_id = add_job
+        puts "JID = #@job_id"
     end
     # 2 - Enqueue a job and return its unique identifier
     def exec_later
-        puts "Exec_later llamado con éxito\n"
+        # puts "Exec_later llamado con éxito\n"
         @job_id = add_job
+        puts "JID = #@job_id"
     end
     # 3 - Enqueue a job in at least <time> seconds
     def exec_in time
         @time = time
-        puts "Exec_in llamado con éxito, time vale: #@time \n"
+        # puts "Exec_in llamado con éxito, time vale: #@time \n"
         @job_id = add_job
+        puts "JID = #@job_id"
     end
 
     # Job ID Counter - Give an unique ID to each job
 
     def add_job ()
         @@no_of_jobs += 1
-        puts "Actualmente hay #@@no_of_jobs jobs"
+        # puts "Actualmente hay #@@no_of_jobs jobs"
         @@no_of_jobs
     end
 end
 
-job = Job_one.new
+class Job_freak_print < Job_print
+
+    # Instance variables
+    @job_id= 0
+    @sync= false
+    @time=0
+
+    def exec_now
+        super
+        puts "May the Force be with you Now\n"
+    end
+   
+    def exec_later
+        super
+        puts "Use the Force\n"
+    end
+
+    def exec_in time
+        super
+        puts "The Force is my ally!\n"
+    end
+end
+
+job = Job_print.new
+job2 = Job_freak_print.new
 job.exec_now
 job.exec_in (5)
 job.exec_later
+puts "Using the Force, please wait...\n"
+sleep(2)
+job2.exec_now
+job2.exec_in (5)
+job2.exec_later
